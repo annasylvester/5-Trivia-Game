cf13a509f8f01c38038bacdb9546a6f781330bd6
annasylvester <annaleesylvester@gmail.com>
2018-12-20 23:56:16 -0500

    Final Commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,19 +2,53 @@
 
     // Variables
     var startScreen;
-    var gameHTML;
+    var generateText;
     var counter = 15;
-    var questionArray = ["What is a group of cats called?", "How similar is a cat's brain to a human's brain?", "About how many different sounds can a cat make?", "What would Egyptian family members do when a family cat died? (Other than mummify the cat of course!)", "What percentage of their lives are cats sleeping??", "Black cats are considered bad luck in America. But in what country are they considered good luck?", "What is the oldest cat breed?", "There is an island where the number of cats to humans is 6 to 1. What country is this in?"];
-    var answerArray = [["Shrewdness", "Clowder", "Herd", "Gang"], ["15%","60%","90%","99%"], ["10", "50", "100", "500"], ["Shave their eyebrows","Mourn for 7 days","Wear sackcloth and ashes","Sacrifice 12 mice"], ["50%", "60%", "70%", "80%"], ["Austrailia","Nepal","Canada","China"], ["Persian", "Tabby", "Scottish Fold", "Egyptian Mau"], ["Philippines","Japan","Greece","Sweden"]];
-    var imageArray = ["<img class='center-block img-right' src='assets/images/clowder.jpg'>", "<img class='center-block img-right' src='assets/images/brain.jpg'>", "<img class='center-block img-right' src='assets/images/meow.jpg'>", "<img class='center-block img-right' src='assets/images/eyebrows.jpg'>", "<img class='center-block img-right' src='assets/images/sleeping.jpg'>", "<img class='center-block img-right' src='assets/images/blackcat.png'>", "<img class='center-block img-right' src='assets/images/egyptian.jpeg'>", "<img class='center-block img-right' src='assets/images/island.jpg'>"];
-    var correctAnswers = ["B. Clowder", "C. 90%", "C. 100", "A. Shave their eyebrows", "C. 70%", "A. Austrailia", "D. Egyptian Mau", "B. Japan"];
+    var questionArray = [
+        "What is a group of cats called?", 
+        "How similar is a cat's brain to a human's brain?", 
+        "About how many different sounds can a cat make?", 
+        "What would Egyptian family members do when a family cat died? (Other than mummify the cat of course!)", 
+        "What percentage of their lives are cats sleeping??", 
+        "Black cats are considered bad luck in America. But in what country are they considered good luck?", 
+        "What is the oldest cat breed?", 
+        "There is an island where the number of cats to humans is 6 to 1. What country is this in?"
+    ];
+    var answerArray = [
+        ["Shrewdness", "Clowder", "Herd", "Gang"], 
+        ["15%","60%","90%","99%"], 
+        ["10", "50", "100", "500"], 
+        ["Shave their eyebrows","Mourn for 7 days","Wear sackcloth and ashes","Sacrifice 12 mice"], 
+        ["50%", "60%", "70%", "80%"], 
+        ["Austrailia","Nepal","Canada","China"], 
+        ["Persian", "Tabby", "Scottish Fold", "Egyptian Mau"], 
+        ["Philippines","Japan","Greece","Sweden"]
+    ];
+    var imageArray = [
+        "<img class='cat-images' src='assets/images/clowder.jpg'>", 
+        "<img class='cat-images' src='assets/images/brain.jpg'>", 
+        "<img class='cat-images' src='assets/images/meow.jpg'>", 
+        "<img class='cat-images' src='assets/images/eyebrows.jpg'>", 
+        "<img class='cat-images' src='assets/images/sleeping.jpg'>", 
+        "<img class='cat-images' src='assets/images/blackcat.png'>", 
+        "<img class='cat-images' src='assets/images/egyptian.jpeg'>", 
+        "<img class='cat-images' src='assets/images/island.jpg'>"
+    ];
+    var correctAnswers = [
+        "B. Clowder", 
+        "C. 90%", 
+        "C. 100", 
+        "A. Shave their eyebrows", 
+        "C. 70%", "A. Austrailia", 
+        "D. Egyptian Mau", 
+        "B. Japan"];
     var questionCounter = 0;
     var selecterAnswer;
-    var theClock;
-    var correctTally = 0;
-    var incorrectTally = 0;
-    var unansweredTally = 0;
-    var clickSound = new Audio("../sound/meow-audio.mp3");
+    var timer;
+    var winCounter = 0;
+    var lossCounter = 0;
+    var unansweredCounter = 0;
+    var clickSound = new Audio("assets/sound/meow-audio.mp3");
     // ------------------------------
 
 $(document).ready(function() {
@@ -31,7 +65,7 @@ $(document).ready(function() {
         clickSound.play();
         generateHTML();
     
-        timerWrapper();
+        timerWait();
     });
 
     $("body").on("click", ".answer", function(event){
@@ -41,12 +75,12 @@ $(document).ready(function() {
         if(selectedAnswer === correctAnswers[questionCounter]) {
             //alert("correct");
     
-            clearInterval(theClock);
+            clearInterval(timer);
             generateWin();
         }
         else {
             //alert("wrong answer!");
-            clearInterval(theClock);
+            clearInterval(timer);
             generateLoss();
         }
     }); // Close .answer click
@@ -59,48 +93,48 @@ $(document).ready(function() {
     });  //  Closes jQuery wrapper
     
     function generateLossDueToTimeOut() {
-        unansweredTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 3000); 
+        unansweredCounter++;
+        generateText = "<p class='timer'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p>Oops! Out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+        $(".mainArea").html(generateText);
+        setTimeout(wait, 4000); 
     }
     
     function generateWin() {
-        correctTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 3000);  //  change to 4000 or other amount
+        winCounter++;
+        generateText = "<p class='timer'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+        $(".mainArea").html(generateText);
+        setTimeout(wait, 4000);  
     }
     
     function generateLoss() {
-        incorrectTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-        $(".mainArea").html(gameHTML);
-        setTimeout(wait, 3000); //  change to 4000 or other amount
+        lossCounter++;
+        generateText = "<p class='timer'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p> Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+        $(".mainArea").html(generateText);
+        setTimeout(wait, 4000);
     }
     
     function generateHTML() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
-        $(".mainArea").html(gameHTML);
+        generateText = "<p class='timer'>Time Remaining: <span class='timer'>15</span></p> <p>" + questionArray[questionCounter] + "</p> <p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+        $(".mainArea").html(generateText);
     }
     
     function wait() {
         if (questionCounter < 7) {
         questionCounter++;
         generateHTML();
-        counter = 30;
-        timerWrapper();
+        counter = 15;
+        timerWait();
         }
         else {
             finalScreen();
         }
     }
     
-    function timerWrapper() {
-        theClock = setInterval(thirtySeconds, 1000);
-        function thirtySeconds() {
+    function timerWait() {
+        timer = setInterval(fifteenSeconds, 1000);
+        function fifteenSeconds() {
             if (counter === 0) {
-                clearInterval(theClock);
+                clearInterval(timer);
                 generateLossDueToTimeOut();
             }
             if (counter > 0) {
@@ -111,18 +145,18 @@ $(document).ready(function() {
     }
     
     function finalScreen() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-        $(".mainArea").html(gameHTML);
+        generateText = "<p class='timer'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p>Annnnddddd here's your results!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + winCounter + "</p>" + "<p>Wrong Answers: " + lossCounter + "</p>" + "<p>Unanswered: " + unansweredCounter + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+        $(".mainArea").html(generateText);
     }
     
     function resetGame() {
         questionCounter = 0;
-        correctTally = 0;
-        incorrectTally = 0;
-        unansweredTally = 0;
+        winCounter = 0;
+        lossCounter = 0;
+        unansweredCounter = 0;
         counter = 15;
         generateHTML();
-        timerWrapper();
+        timerWait();
     }
     
 
